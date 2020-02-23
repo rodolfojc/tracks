cfe1ee6e3bbc61d6bd8d9e5e007c32005b0c5601
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-23 21:27:48 +0000

    Navigation to Signin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext} from '../context/AuthContext';
@@ -37,6 +37,11 @@ const SignupScreen = ({ navigation }) => {
                     onPress={() => signup({ email, password })} 
                     title="Sign Up" />
             </Spacer>
+            <TouchableOpacity onPress={() =>navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>Already an account? Sing in instead here!</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -60,6 +65,9 @@ const styles = StyleSheet.create({
         color: 'red',
         marginLeft: 15,
         marginTop: 15
+    },
+    link: {
+        color: 'blue'
     }
 });
 
