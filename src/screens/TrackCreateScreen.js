4c019ff128ae2e31a268b4d5736b8aa60c4fdefc
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-26 20:50:52 +0000

    Requesting Location Permissions

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { Text } from 'react-native-elements';
 import Map from '../components/Map';
+import { requestPermissionsAsync} from 'expo-location';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState('');
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+        } catch (err) {
+            setErr(err);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2> Create Track!!</Text>
             <Map />
+            {err ? <Text>Please enable location access!!</Text> : null}
         </SafeAreaView>
     );
 };
